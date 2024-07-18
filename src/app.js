f4404e2328e9f704a363c62d5ad7bf5535fab31e
Caio Oliveira <caio.oliveira07@gmail.com>
2024-07-18 01:52:18 -0300

    CRUD implementado

--- src/app.js
+++ src/app.js
@@ -14,6 +14,12 @@ const livros = [
     }
 ]
 
+function buscaLivro(id){
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    });
+}
+
 app.get('/', (req, res) => {
     res.status(200).send('Curso de Node.js');
 });
@@ -22,9 +28,26 @@ app.get('/livros', (req, res) => {
     res.status(200).json(livros);
 });
 
+app.get('/livros/:idLivro', (req, res) => {
+   const index = buscaLivro(req.params.idLivro);
+   res.status(200).json(livros[index]);
+});
+
 app.post('/livros', (req, res) => { 
     livros.push(req.body);
     res.status(201).send('Livro adicionado com sucesso');
+});
+
+app.put('/livros/:idLivro', (req, res) => {
+    const index = buscaLivro(req.params.idLivro);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).send('Livro atualizado com sucesso');
+});
+
+app.delete('/livros/:idLivro', (req, res) => {
+    const index = buscaLivro(req.params.idLivro);
+    livros.splice(index, 1);
+    res.status(200).send('Livro removido com sucesso');
 });
 
 export default app;
