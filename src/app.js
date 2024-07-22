13132abd1d9000e1c19c774bb700c6c1df73917a
Caio Oliveira <caio.oliveira07@gmail.com>
2024-07-21 23:57:06 -0300

    Aplicação configurada e conectada ao Mongo

--- src/app.js
+++ src/app.js
@@ -1,31 +1,28 @@
 import express from 'express';
+import conectaNaDb from './config/dbConnect.js';
+import livro from './models/livro.js';
+
+const conexao = await conectaNaDb();
+// Abaixo está sendo feita a conexão com o MongoDB. O método connect recebe a URL de conexão com o banco de dados. O método retorna uma Promise, que é aguardada com o await.
+conexao.on('error', (erro) => { 
+    console.error('Erro ao conectar no MongoDB', erro);
+});
+// Abaixo está sendo feito um tratamento de erro para o evento de erro da conexão. O método on recebe o nome do evento e uma função de callback que será executada quando o evento ocorrer.
+conexao.once('open', () => {
+    console.log('Conectado no MongoDB');
+});
 
 const app = express();
 app.use(express.json());
 
-const livros = [
-    {
-        id: 1,
-        titulo: 'O Senhor dos Anéis'
-    },
-    {
-        id: 2,
-        titulo: 'Harry Potter'
-    }
-]
-
-function buscaLivro(id){
-    return livros.findIndex(livro => {
-        return livro.id === Number(id);
-    });
-}
 
 app.get('/', (req, res) => {
     res.status(200).send('Curso de Node.js');
 });
 
-app.get('/livros', (req, res) => {
-    res.status(200).json(livros);
+app.get('/livros', async (req, res) => {
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros);
 });
 
 app.get('/livros/:idLivro', (req, res) => {
---
+++ src/models/livro.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+// Abaixo está sendo criado um schema para o livro, que será utilizado para criar um modelo de livro.
+const livroSchema = new mongoose.Schema({
+    id: {type: mongoose.Schema.Types.ObjectId},
+    titulo: {type: String, required: true},
+    editora: {type: String},
+    preco: {type: Number},
+    paginas: {type: Number}
+},{versionKey: false});
+
+// Abaixo está sendo criado um modelo de livro, que será utilizado para realizar operações no banco de dados. Os parâmetros são o nome da coleção e o schema.
+const livros = mongoose.model('livros', livroSchema);
+
+export default livros;
